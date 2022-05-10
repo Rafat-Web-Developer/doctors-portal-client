af6166a6253b61efcf0d6ef1f8ad31bad73edb9b
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-10 07:03:21 +0600

    Fix a design bug in banner

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -5,7 +5,11 @@ const Banner = () => {
   return (
     <div class="hero min-h-screen">
       <div class="hero-content flex-col lg:flex-row-reverse">
-        <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
+        <img
+          src={chair}
+          class="max-w-sm rounded-lg shadow-2xl"
+          alt="Banner_image"
+        />
         <div>
           <h1 class="text-5xl font-bold">Box Office News!</h1>
           <p class="py-6">
