41dd1319ba2ed4b67f3332a45d809660d3fc72c7
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-10 09:06:51 +0600

    Creaate exception component

---
+++ src/Pages/Home/Exceptional.js
@@ -0,0 +1,30 @@
+import React from "react";
+import treatment from "../../assets/images/treatment.png";
+
+const Exceptional = () => {
+  return (
+    <div class="mb-20 card card-side bg-base-100 shadow-xl grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4">
+      <figure>
+        <img src={treatment} alt="treatment_img" style={{ height: "476px" }} />
+      </figure>
+      <div class="card-body">
+        <div>
+          <h2 class="card-title text-5xl mb-10">
+            Exceptional Dental Care, on Your Terms
+          </h2>
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsumis that it has a more-or-less normal
+            distribution of letters,as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page
+          </p>
+          <button class="btn btn-primary mt-10">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Exceptional;
