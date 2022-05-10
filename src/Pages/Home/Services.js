5aa4d8db154a60a6de2f8ff00f8da06774164b42
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-10 08:39:31 +0600

    Create services component and design it

---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Service from "./Service";
+import cavity from "../../assets/images/cavity.png";
+import fluoride from "../../assets/images/fluoride.png";
+import whitening from "../../assets/images/whitening.png";
+
+const Services = () => {
+  const services = [
+    {
+      _id: 1,
+      name: "Fluoride Treatment",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      img: fluoride,
+    },
+    {
+      _id: 2,
+      name: "Cavity Filling",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      img: cavity,
+    },
+    {
+      _id: 3,
+      name: "Teeth Whitening",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      img: whitening,
+    },
+  ];
+
+  return (
+    <div className="my-28">
+      <div className="text-center">
+        <h1 className="text-xl font-bold text-primary">Our Services</h1>
+        <p className="text-4xl">Services We Provide</p>
+      </div>
+      <div className="mt-20 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {services.map((service) => (
+          <Service key={service._id} service={service}></Service>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
