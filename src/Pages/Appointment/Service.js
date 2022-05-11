c191cd79db4af391e61a93a5b4d67ae439721378
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-11 07:21:07 +0600

    Add condition in appoinment service

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -1,14 +1,26 @@
 import React from "react";
 
 const Service = ({ service }) => {
-  const { name, slot } = service;
+  const { name, slots } = service;
   return (
     <div class="card lg:max-w-lg bg-base-100 shadow-xl">
       <div class="card-body text-center">
         <h2 class="text-secondary text-xl font-bold">{name}</h2>
-        <p>Slot</p>
+        {slots.length ? (
+          <span>{slots[0]}</span>
+        ) : (
+          <span className="text-error font-bold">Try Next Day</span>
+        )}
+        <span>
+          {slots.length} {slots.length > 1 ? "slots" : "slot"} available
+        </span>
         <div class="card-actions justify-center">
-          <button class="btn btn-secondary text-white">Book Appointment</button>
+          <button
+            class="btn btn-secondary text-white"
+            disabled={slots.length === 0}
+          >
+            Book Appointment
+          </button>
         </div>
       </div>
     </div>
