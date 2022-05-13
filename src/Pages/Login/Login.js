e46c33ce89e6bdc57abd9a380a37a83798a15c32
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-13 10:08:24 +0600

    Design login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,9 +2,36 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <h1>This is login</h1>
-    </div>
+    <section className="flex justify-center items-center h-screen">
+      <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <h1 className="text-xl font-bold text-center my-2">Login</h1>
+        <div class="card-body">
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text font-bold">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="email"
+              class="input input-bordered"
+            />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text font-bold">Password</span>
+            </label>
+            <input
+              type="text"
+              placeholder="password"
+              class="input input-bordered"
+            />
+          </div>
+          <div class="form-control mt-6">
+            <button class="btn btn-secondary text-white">Login</button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
