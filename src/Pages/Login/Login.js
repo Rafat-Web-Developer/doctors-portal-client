92f0302294bbbd862bd526e586e65fceec770fba
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-13 11:02:19 +0600

    Use react hooks form and check validation

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,36 +1,98 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <section className="flex justify-center items-center h-screen">
-      <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100"
+      >
         <h1 className="text-xl font-bold text-center my-2">Login</h1>
-        <div class="card-body">
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text font-bold">Email</span>
+        <div className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-bold">Email</span>
             </label>
             <input
               type="text"
               placeholder="email"
-              class="input input-bordered"
+              className="input input-bordered"
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Email is required.",
+                },
+                pattern: {
+                  value: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+                  message: "Provide a valid email",
+                },
+              })}
             />
+            <label className="label">
+              {errors.email?.type === "required" && (
+                <span className="label-text-alt font-bold text-error">
+                  {errors.email.message}
+                </span>
+              )}
+              {errors.email?.type === "pattern" && (
+                <span className="label-text-alt font-bold text-error">
+                  {errors.email.message}
+                </span>
+              )}
+            </label>
           </div>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text font-bold">Password</span>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-bold">Password</span>
             </label>
             <input
               type="text"
               placeholder="password"
-              class="input input-bordered"
+              className="input input-bordered"
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: "Email is required.",
+                },
+                minLength: {
+                  value: 6,
+                  message: "Password must have 6 character.",
+                },
+              })}
+            />
+            <label className="label">
+              {errors.password?.type === "required" && (
+                <span className="label-text-alt font-bold text-error">
+                  {errors.password.message}
+                </span>
+              )}
+              {errors.password?.type === "minLength" && (
+                <span className="label-text-alt font-bold text-error">
+                  {errors.password.message}
+                </span>
+              )}
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <input
+              className="btn btn-secondary text-white"
+              type="submit"
+              value="Login"
             />
           </div>
-          <div class="form-control mt-6">
-            <button class="btn btn-secondary text-white">Login</button>
-          </div>
         </div>
-      </div>
+      </form>
     </section>
   );
 };
