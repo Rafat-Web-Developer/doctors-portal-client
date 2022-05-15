e9787dcb03fe478bc68627df3298259883b08031
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-16 00:54:48 +0600

    Resolve reload problem

--- src/Pages/Shared/RequireAuth.js
+++ src/Pages/Shared/RequireAuth.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate } from "react-router-dom";
+import Loading from "../Shared/Loading";
 import auth from "../../firebase.init";
 
 const RequireAuth = ({ children }) => {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
 
   if (!user) {
     return <Navigate to="/login"></Navigate>;
