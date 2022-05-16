7c14c3d591401f0cd62f42a555baf9944960fab8
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-17 01:19:31 +0600

    load all bookings data

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -7,14 +7,14 @@ const Dashboard = () => {
       <input id="dashboardSidebar" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content p-3">
         {/* <!-- Page content here --> */}
-        <h1 className="text-primary font-bold text-xl md:text-2xl">
+        <h1 className="text-primary font-bold text-xl md:text-2xl mb-5">
           Welcome to your Dashboard
         </h1>
         <Outlet />
       </div>
       <div className="drawer-side">
         <label htmlFor="dashboardSidebar" className="drawer-overlay"></label>
-        <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+        <ul className="menu p-4 overflow-y-auto w-64 bg-base-100 text-base-content">
           {/* <!-- Sidebar content here --> */}
           <li>
             <Link to="/dashboard">My Appointment</Link>
--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const MyAppointments = () => {
+  const [user] = useAuthState(auth);
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/bookings?patientEmail=${user?.email}`)
+        .then((res) => res.json())
+        .then((data) => setBookings(data));
+    }
+  }, []);
+
   return (
-    <div>
-      <h1>My All Appointments</h1>
+    <div className="overflow-x-auto">
+      <table className="table table-compact w-full">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Treatment Name</th>
+            <th>Date</th>
+            <th>Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bookings.map((b, index) => (
+            <tr key={b._id}>
+              <th>{index + 1}</th>
+              <td>{b.treatment_name}</td>
+              <td>{b.date}</td>
+              <td>{b.slot}</td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <th>#</th>
+            <th>Treatment Name</th>
+            <th>Date</th>
+            <th>Time</th>
+          </tr>
+        </tfoot>
+      </table>
     </div>
   );
 };
