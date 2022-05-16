7ed7e7de083fb07f6309e311866cf96971eb34e4
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-16 23:51:38 +0600

    Use React Query for reload services

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,19 +1,28 @@
 import { format } from "date-fns";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
 import BookingModal from "./BookingModal";
 import Service from "./Service";
+import Loading from "../Shared/Loading";
 
 const AvailableAppointment = ({ date }) => {
-  const [services, setServices] = useState([]);
-  const [treatment, setTreatment] = useState({});
+  const [treatment, setTreatment] = useState(null);
 
   const formattedDate = format(date, "PP");
 
-  useEffect(() => {
-    fetch(`http://localhost:5000/available?date=${formattedDate}`)
-      .then((res) => res.json())
-      .then((data) => setServices(data));
-  }, [formattedDate]);
+  const {
+    data: services,
+    isLoading,
+    refetch,
+  } = useQuery(["available", formattedDate], () =>
+    fetch(`http://localhost:5000/available?date=${formattedDate}`).then((res) =>
+      res.json()
+    )
+  );
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
 
   return (
     <section className="mb-28">
@@ -34,6 +43,7 @@ const AvailableAppointment = ({ date }) => {
           date={date}
           treatment={treatment}
           setTreatment={setTreatment}
+          refetch={refetch}
         ></BookingModal>
       )}
     </section>
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -4,7 +4,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
 import { toast } from "react-toastify";
 
-const BookingModal = ({ date, treatment, setTreatment }) => {
+const BookingModal = ({ date, treatment, setTreatment, refetch }) => {
   const { _id, name, slots } = treatment;
   const [user] = useAuthState(auth);
   const formattedDate = format(date, "PP");
@@ -35,11 +35,11 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
         console.log(data);
         if (data.success) {
           toast("Booking Successfully Alhamdulillah.");
-          setTreatment(null);
         } else {
           toast.error(data.message);
-          setTreatment(null);
         }
+        setTreatment(null);
+        refetch();
       });
   };
 
