ad67bfd4b28f962339a288cc4a60199b7aa175f0
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-17 23:35:33 +0600

    Verify user token is valid or not

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -1,15 +1,30 @@
+import { signOut } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 
 const MyAppointments = () => {
   const [user] = useAuthState(auth);
   const [bookings, setBookings] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (user) {
-      fetch(`http://localhost:5000/bookings?patientEmail=${user?.email}`)
-        .then((res) => res.json())
+      fetch(`http://localhost:5000/bookings?patientEmail=${user?.email}`, {
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+        .then((res) => {
+          if (res.status === 401 || res.status === 403) {
+            signOut(auth);
+            localStorage.removeItem("accessToken");
+            navigate("/");
+          }
+          res.json();
+        })
         .then((data) => setBookings(data));
     }
   }, []);
@@ -26,12 +41,12 @@ const MyAppointments = () => {
           </tr>
         </thead>
         <tbody>
-          {bookings.map((b, index) => (
+          {bookings?.map((b, index) => (
             <tr key={b._id}>
               <th>{index + 1}</th>
-              <td>{b.treatment_name}</td>
-              <td>{b.date}</td>
-              <td>{b.slot}</td>
+              <td>{b?.treatment_name}</td>
+              <td>{b?.date}</td>
+              <td>{b?.slot}</td>
             </tr>
           ))}
         </tbody>
