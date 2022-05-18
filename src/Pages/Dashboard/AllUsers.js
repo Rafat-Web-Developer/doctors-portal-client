46c03e099055ef858899580301b54dc685d68c4d
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-18 06:32:32 +0600

    Emplement make user and verify admin or not

--- src/Pages/Dashboard/AllUsers.js
+++ src/Pages/Dashboard/AllUsers.js
@@ -4,8 +4,17 @@ import Loading from "../Shared/Loading";
 import UsersRow from "./UsersRow";
 
 const AllUsers = () => {
-  const { data: users, isLoading } = useQuery("users", () =>
-    fetch("http://localhost:5000/users").then((res) => res.json())
+  const {
+    data: users,
+    isLoading,
+    refetch,
+  } = useQuery("users", () =>
+    fetch("http://localhost:5000/users", {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
   );
   if (isLoading) {
     return <Loading></Loading>;
@@ -13,23 +22,30 @@ const AllUsers = () => {
   return (
     <div>
       <h1 className="text-2xl font-bold mb-5">All Users {users.length}</h1>
-      <div class="overflow-x-auto">
-        <table class="table table-compact w-full">
+      <div className="overflow-x-auto">
+        <table className="table table-compact w-full">
           <thead>
             <tr>
               <th>#</th>
               <th>User's Email</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
             {users.map((user, index) => (
-              <UsersRow key={user._id} user={user} index={index}></UsersRow>
+              <UsersRow
+                key={user._id}
+                user={user}
+                index={index}
+                refetch={refetch}
+              ></UsersRow>
             ))}
           </tbody>
           <tfoot>
             <tr>
               <th>#</th>
               <th>User's Email</th>
+              <th>Action</th>
             </tr>
           </tfoot>
         </table>
--- src/Pages/Dashboard/UsersRow.js
+++ src/Pages/Dashboard/UsersRow.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { toast } from "react-toastify";
 
-const UsersRow = ({ user, index }) => {
+const UsersRow = ({ user, index, refetch }) => {
+  const { email, role } = user;
+  const makeAdmin = () => {
+    fetch(`http://localhost:5000/user/admin/${email}`, {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => {
+        if (res.status === 403) {
+          toast.error("Failed to Make an admin");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          refetch();
+          toast.success(`Successfully made an admin`);
+        }
+      });
+  };
   return (
-    <tr>
+    <tr className="font-bold">
       <th>{index + 1}</th>
-      <td>{user?.email}</td>
+      <td>{email}</td>
+      <td>
+        {role !== "admin" && (
+          <button
+            onClick={makeAdmin}
+            className="btn btn-xs btn-secondary font-bold text-white"
+          >
+            Make Admin
+          </button>
+        )}
+      </td>
     </tr>
   );
 };
